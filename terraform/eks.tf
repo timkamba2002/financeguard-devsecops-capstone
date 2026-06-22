3d63ad020f0618a49e6920667c0c3ea28292537a
Timothy Kamba <timothykamba2002@gmail.com>
2026-06-22 18:52:04 -0400

    eks updated policy for ecr

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -5,8 +5,8 @@ resource "aws_iam_role" "eks_cluster" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
+      Action  = "sts:AssumeRole"
+      Effect  = "Allow"
       Principal = { Service = "eks.amazonaws.com" }
     }]
   })
@@ -52,15 +52,15 @@ resource "aws_eks_fargate_profile" "main" {
   }
 }
 
-# Fargate Pod Execution Role
+# Fargate Pod Execution Role + ECR Permissions
 resource "aws_iam_role" "eks_fargate_pod_execution" {
   name = "financeguard-fargate-pod-role-${var.environment}"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
+      Action  = "sts:AssumeRole"
+      Effect  = "Allow"
       Principal = { Service = "eks-fargate-pods.amazonaws.com" }
     }]
   })
@@ -71,7 +71,18 @@ resource "aws_iam_role_policy_attachment" "fargate_pod_execution_policy" {
   role       = aws_iam_role.eks_fargate_pod_execution.name
 }
 
-# Small EC2 Node Group for System Workloads (Argo CD, etc.)
+# ECR Pull for Fargate (Critical Fix)
+resource "aws_iam_role_policy_attachment" "fargate_ecr_readonly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_fargate_pod_execution.name
+}
+
+resource "aws_iam_role_policy_attachment" "fargate_ecr_custom" {
+  policy_arn = aws_iam_policy.ecr_pull.arn
+  role       = aws_iam_role.eks_fargate_pod_execution.name
+}
+
+# Small EC2 Node Group for System Workloads
 resource "aws_eks_node_group" "system" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = "system-nodes"
@@ -96,15 +107,15 @@ resource "aws_eks_node_group" "system" {
   ]
 }
 
-# Node Group IAM Role (for system nodes)
+# Node Group IAM Role
 resource "aws_iam_role" "eks_nodes" {
   name = "financeguard-eks-node-role-${var.environment}"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
+      Action  = "sts:AssumeRole"
+      Effect  = "Allow"
       Principal = { Service = "ec2.amazonaws.com" }
     }]
   })
